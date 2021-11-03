9a4f86cb304ab451b3c58d1cec3eb24b9453f17d
Erivaldo21904177 <a21904177@alunos.ulht.pt>
2021-11-03 15:57:51 +0000

    criadas interfaces base de classes

--- src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
@@ -2,5 +2,21 @@ package pt.ulusofona.lp2.deisiGreatGame;
 
 public class Programmer {
 
+    int getId(){
+        return 0;
+    }
+
+    String getName(){
+        return "";
+    }
+
+    ProgrammerColor getColor(){
+        return null;
+    }
+
+   public String toString(){
+        return "";
+    }
+
 
 }
