b4da5a18a2e527d3f1992ab94d14190a5eb85a76
Erivaldo21904177 <a21904177@alunos.ulht.pt>
2021-11-09 17:25:28 +0000

    primeira version a funcionar

--- src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/GameManager.java
@@ -1,45 +1,138 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
 import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameManager {
+
+    private ArrayList<Programmer> jogadores;
+    private int boardSize;
+    private int currentPlayer;
 
     public GameManager(){
 
     }
 
     public boolean createInitialBoard(String[][] playerInfo, int boardSize){
+        //Verificacao de numero jogadores
+        if (playerInfo.length < 2 || playerInfo.length > 4){
+            return false;
+        }
+
+
+        //Verificacao do tabuleiro tamanho
+        if (boardSize < playerInfo.length*2) {
+            return false;
+        }
+
+        this.jogadores = new ArrayList<Programmer>();
+        this.boardSize = boardSize;
+        this.currentPlayer = 0;
+
+        for(int i=0;i<playerInfo.length;i++){
+            int id_jogador = Integer.parseInt(playerInfo[i][0]);
+            String nome = playerInfo[i][1];
+            String lista_linguagens = playerInfo[i][2];
+            String cor = playerInfo[i][3];
+
+            //id numero inteiro positivo
+            if (id_jogador < 0) {
+                return false;
+            }
+
+            for(int j=0;j<playerInfo.length;j++){
+
+                //ids programadores repetidos
+                if (j != i && Integer.parseInt(playerInfo[j][0]) == id_jogador){
+                    return false;
+                }
+
+                //Nao podem haver cores iguais
+                if (j != i && playerInfo[j][3] == cor) {
+                    return false;
+                }
+            }
+
+            //os nomes dos porgramadaores
+            if(nome == null||nome == "") {
+                return false;
+            }
+
+            //cor dos jogadores
+            if (cor != "Purple" && cor != "Green" && cor != "Brown" && cor != "Blue") {
+                return false;
+            }
+
+            //Adicionar jogador
+            this.jogadores.add( new Programmer(id_jogador,nome,cor,lista_linguagens) );
+        }
+
         return true;
     }
 
     public String getImagePng(int position){
+        if(position >= this.boardSize || position < 0){
+            return null;
+        }
 
-        return "";
+        if(position == this.boardSize - 1){
+            return "glory.png";
+        }
+
+        return null;//Integer.toString(position) + ".png";
     }
 
     public ArrayList<Programmer> getProgrammers(){
-        return null;
+        return this.jogadores;
     }
 
     public ArrayList<Programmer> getProgrammers(int position){
-        return null;
+        ArrayList<Programmer> k = new ArrayList<Programmer>();
+
+        for(Programmer x : this.jogadores){
+            if(x.getPos() == position) {
+                k.add(x);
+            }
+        }
+
+        return k;
     }
 
     public int getCurrentPlayerID(){
-        return 0;
+        return this.jogadores.get(this.currentPlayer).getId();
     }
 
     public boolean moveCurrentPlayer(int nrPositions){
-        return false;
+
+        if(nrPositions < 1 || nrPositions > 6)
+            return false;
+        Programmer x = this.jogadores.get(this.currentPlayer);
+
+        if(x.getPos()+nrPositions < this.boardSize - 1)
+            x.setPos(x.getPos()+nrPositions);
+        else
+            x.setPos(this.boardSize - x.getPos()+nrPositions - this.boardSize);
+
+        return true;
     }
 
     public boolean gameIsOver(){
+        if(this.getProgrammers(this.boardSize-1).size() >= 1){
+            return true;
+        }
+
         return false;
     }
 
     public ArrayList<String> getGameResults(){
-        return null;
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(Programmer x : this.jogadores){
+            ret.add(x.toString());
+        }
+
+        return ret;
     }
 
     public JPanel getAuthorsPanel(){
--- src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Programmer.java
@@ -1,21 +1,81 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
+import java.util.ArrayList;
+
 public class Programmer {
 
-    int getId(){
-        return 0;
+    private int id;
+    private String name;
+    private ProgrammerColor color;
+    private String[] linguagens;
+    private int pos;
+    private String estado;
+
+    public Programmer(int id, String name, String color, String linguagens){
+        this.id = id;
+        this.name = name;
+        switch(color){
+
+            case "Purple":
+                this.color = ProgrammerColor.PURPLE;
+                break;
+            case "Green":
+                this.color = ProgrammerColor.GREEN;
+                break;
+            case "Brown":
+                this.color = ProgrammerColor.BROWN;
+                break;
+            case "Blue":
+                this.color = ProgrammerColor.BLUE;
+                break;
+        }
+
+        this.linguagens = linguagens.split(";");
+
+        this.pos = 0;
+        this.estado = "Em Jogo";
     }
 
-    String getName(){
-        return "";
+    public int getId(){
+        return this.id;
     }
 
-    ProgrammerColor getColor(){
-        return null;
+
+    public int getPos(){
+
+        return this.pos;
     }
 
-   public String toString(){
-        return "";
+    public void setPos(int pos){
+        this.pos = pos;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ProgrammerColor getColor(){
+        return this.color;
+    }
+    private String linguagensToString(){
+        String ret = "";
+        for(String k : this.linguagens){
+
+            if (ret.equals("")) {
+                ret = k;
+            } else {
+                ret = ret + "; " + k;
+            }
+        }
+
+        return ret;
+    }
+    public String toString(){
+        return Integer.toString(this.id) + " | "
+                + this.name + " | " + Integer.toString(this.pos) +
+                " | " + this.linguagensToString() + " | " +
+                this.estado;
+
     }
 
 
