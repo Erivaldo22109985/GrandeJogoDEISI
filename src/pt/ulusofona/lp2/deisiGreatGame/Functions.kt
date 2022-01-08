d70be040141e70063f4871d375537b56995da278
Erivaldo21904177 <a21904177@alunos.ulht.pt>
2022-01-08 15:36:00 +0000

    3 parte

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -27,10 +27,23 @@ fun get(g: GameManager, l: List<String>): String?{
     var arg = l.get(0);
     when(arg){
         "PLAYER" -> return get_player(g,l);
+        "PLAYERS_BY_LANGUAGE" -> get_player_by_language(g,l);
     }
     return null;
 }
+fun get_player_by_language(g: GameManager, l: List<String>): String?{
+    val x = l.get(1);
+    val k = "dwq";
+    var ret:String = "";
 
+    k.split("," , ignorecase= true, limit=0);
+
+    g.getProgrammers(true).forEach {
+
+    }
+
+    return ret;
+}
 fun get_player(g: GameManager, l: List<String>): String?{
     var ret:String = "Inexistent player";
     g.getProgrammers(true).forEach {
